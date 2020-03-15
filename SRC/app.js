4e57ee7ed44f624064a461e8ed92a6ef60e70055
Matan Kadosh <kadosh10001@gmail.com>
2020-03-15 20:18:05 +0200

    * better code reuse * change methods and plugin names * update zip

--- SRC/app.js
+++ SRC/app.js
@@ -1,35 +1,49 @@
 const config = require("./config");
 const mapExecutionService = require("../../../api/services/map-execution.service");
 const Trigger = require("../../../api/models/map-trigger.model");
+
 module.exports = {
-    prWebhook: function (req, res) {
-        controllerfunctionPR(req,res)
-    },
-    pushWebhook: function(req, res){
-        controllerfunctionPush(req,res)
-    }
+    prWebhook: controllerfunctionPR,
+    pushWebhook: controllerfunctionPush
 }
 
-function controllerfunctionPR (req, res) {
-    let push = req.body;
-    
-    if(!push.resource.repository) {
-        console.log('Repo not found')
-        return res.send('repo not found')
-    }
-    
-    let repositoryURL = push.resource.repository.remoteUrl
-    let targetBranch = push.resource.targetRefName.slice(11); //Get target branch name
-    let sourceBranch = push.resource.sourceRefName.slice(11); //Get source branch name
-    let requestBy = push.resource.createdBy.displayName;
-    let reviewer = push.resource.reviewers[push.resource.reviewers.length - 1].displayName;
+function findTriggers(body, validatationFn, startParams, req, res){
     Trigger.find({ plugin: config.name }).then((triggers) => {
         console.log(`Found ${triggers.length} triggers`);
         res.send('OK');
         triggers.forEach(trigger=>{  
-            execTriggerPR(trigger,{push, repositoryURL, targetBranch, sourceBranch, requestBy, reviewer},req.io);
+            validatationFn(trigger,startParams)
+            .then(exec(trigger, body, req.io))
+            .catch(console.error);
         });
     }).catch((error) => res.send(error))
+}
+
+function exec(trigger, body, io){
+    return ()=>{
+        console.log(trigger.map);
+        let message = trigger.name + ' - Started by Azure Git trigger';
+        /*if (body.sender && body.sender.login) {
+            message += ` (body by ${body.sender.login}`
+        }*/
+        console.log(`******** Azure Git: executing map ${trigger.map} ********`);
+        mapExecutionService.execute(trigger.map, null, io, {config : trigger.configuration}, message, body);
+    }
+}
+
+function controllerfunctionPR (req, res) {
+    let body = req.body;
+    
+    if(!body.resource.repository) {
+        console.log('Repo not found')
+        return res.send('repo not found')
+    }
+    
+    let repositoryURL = body.resource.repository.remoteUrl
+    let targetBranch = body.resource.targetRefName.slice(11); //Get target branch name
+    let sourceBranch = body.resource.sourceRefName.slice(11); //Get source branch name
+    
+    findTriggers(body, validateTriggerPR, {repositoryURL, targetBranch, sourceBranch},req, res);
 }
 
 function controllerfunctionPush(req,res){
@@ -42,31 +56,22 @@ function controllerfunctionPush(req,res){
     
     let repositoryURL = push.resource.repository.remoteUrl
     let pushBranch = push.resource.refUpdates[push.resource.refUpdates.length - 1].name.slice(11); //Get target branch name
-    let pushedBy = push.resource.pushedBy.displayName;
-    Trigger.find({ plugin: config.name }).then((triggers) => {
-        console.log(`Found ${triggers.length} triggers`);
-        res.send('OK');
-        triggers.forEach(trigger=>{  
-            execTriggerPush(trigger,{push, repositoryURL, pushBranch, pushedBy},req.io);
-        });
-    }).catch((error) => res.send(error))
+    
+    findTriggers(push, validateTriggerPush, {repositoryURL, pushBranch},req, res);
 }
 
-function execTriggerPR(trigger, {push, repositoryURL, targetBranch, sourceBranch, requestBy, reviewer}, io){
-    new Promise((resolve, reject) => {
+function validateTriggerPR(trigger, {repositoryURL, targetBranch, sourceBranch}){
+    return new Promise((resolve, reject) => {
         
-        //const triggerSecret = trigger.params.find(o => o.name === 'SECRET');
         const triggerRepoUrl = trigger.params.find(o => o.name === 'REPO_URL');
         const toBranch = trigger.params.find(o => o.name === 'TO_BRANCH');
         const fromBranch = trigger.params.find(o => o.name === 'FROM_BRANCH');
-        const requestedBy = trigger.params.find(o => o.name === 'REQUESTED_BY');
-        const reviewerName = trigger.params.find(o => o.name === 'REVIEWER_NAME');
+        
         /**
          * Check if the Repo URL is provided (else consider as ANY)
          * Check that the Repo URL is the same as provided by the Trigger and if not provided 
         */
         if (triggerRepoUrl.value && repositoryURL !== triggerRepoUrl.value) {
-            console.log(url, triggerRepoUrl.value);
             return reject("Not same repo");
         }
 
@@ -84,35 +89,20 @@ function execTriggerPR(trigger, {push, repositoryURL, targetBranch, sourceBranch
             return reject("Not matching target branch")
         }
 
-        else {
-            return resolve();
-        }
-
-    }).then(() => {
-        console.log(trigger.map);
-        let message = trigger.name + ' - Started by Azure Git trigger';
-        /*if (push.sender && push.sender.login) {
-            message += ` (push by ${push.sender.login}`
-        }*/
-        console.log(`******** Azure Git: executing map ${trigger.map} ********`);
-        mapExecutionService.execute(trigger.map, null, io, {config : trigger.configuration}, message, push);
-    }).catch(err=>{
-        console.error(err);
+        return resolve();
     })
 }
 
-function execTriggerPush(trigger, {push, repositoryURL, pushBranch, pushedBy}, io){
-    new Promise((resolve, reject) => {
-        //const triggerSecret = trigger.params.find(o => o.name === 'SECRET');
+function validateTriggerPush(trigger, {repositoryURL, pushBranch}){
+    return new Promise((resolve, reject) => {
         const triggerRepoUrl = trigger.params.find(o => o.name === 'REPO_URL');
         const triggerPushBranch = trigger.params.find(o => o.name === 'PUSH_BRANCH');
-        const triggerPushedBy = trigger.params.find(o => o.name === 'PUSHED_BY');
+
         /**
          * Check if the Repo URL is provided (else consider as ANY)
          * Check that the Repo URL is the same as provided by the Trigger and if not provided 
         */
         if (triggerRepoUrl.value && repositoryURL !== triggerRepoUrl.value) {
-            console.log(url, triggerRepoUrl.value);
             return reject("Not same repo");
         }
 
@@ -123,19 +113,7 @@ function execTriggerPush(trigger, {push, repositoryURL, pushBranch, pushedBy}, i
                 return reject("Not matching pushed branch")
         }
 
-        else {
-            return resolve();
-        }
-
-    }).then(() => {
-        console.log(trigger.map);
-        let message = trigger.name + ' - Started by Azure Git trigger';
-        /*if (push.sender && push.sender.login) {
-            message += ` (push by ${push.sender.login}`
-        }*/
-        console.log(`******** Azure Git: executing map ${trigger.map} ********`);
-        mapExecutionService.execute(trigger.map, null, io, {config : trigger.configuration}, message, push);
-    }).catch(err=>{
-        console.error(err);
-    })
+        return resolve();
+    });
 }
+
